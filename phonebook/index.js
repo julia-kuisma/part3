2a25d6490a95eac52853216894a19dfbc2519733
Julia Kuisma <julia.kuisma@virnex.fi>
2025-11-05 10:10:38 +0200

    3.3. step

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -31,6 +31,13 @@ app.get('/api/persons', (request, response) => {
 app.get('/info', (request, response) => {
     const now = new Date();
     response.send("<p>Phonebook has info for "+persons.length+" people</p><p>"+now+"</p>")
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
