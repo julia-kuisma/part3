883a4b7b900c3ccf99be19cf910f5a9d462d3c1b
Julia Kuisma <julia.kuisma@virnex.fi>
2025-11-06 19:41:02 +0200

    3.7. Step

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 let persons = [
     { 
